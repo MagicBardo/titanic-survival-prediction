dfaafa6f9f49f4c8bae3f2b285a7a27f24c43d38
MagicBardo <theodor.b.klotz@gmail.com>
2026-07-26 18:35:55 +0200

    corrected get root path + updated .gitignore with models and logs folder

--- src/titanic_ml/paths.py
+++ src/titanic_ml/paths.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 
 DATA = PROJECT_ROOT / "data"
 RAW_DATA = DATA / "raw"
